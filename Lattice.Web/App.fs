812b7c553f531efee8f81d81e4a257c5ede6189d
FinOCE <me@itsf.in>
2025-02-10 14:13:41 +1000

    Add project for Lattice web app

---
+++ Lattice.Web/App.fs
@@ -0,0 +1,15 @@
+﻿module Lattice.Web
+
+open Browser.Dom
+open Feliz
+
+[<ReactComponent>]
+let App () = React.fragment [
+    Html.div [
+        Html.text "Hello world"
+    ]
+]
+
+ReactDOM
+    .createRoot(document.getElementById "root")
+    .render(App())
