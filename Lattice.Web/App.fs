9b234e972cbdd624bf6188d6821369c88c9b3ca0
FinOCE <me@itsf.in>
2025-02-10 14:45:31 +1000

    Setup feliz router to handle web page routing

--- Lattice.Web/App.fs
+++ Lattice.Web/App.fs
@@ -2,14 +2,23 @@
 
 open Browser.Dom
 open Feliz
+open Feliz.Router
 
 [<ReactComponent>]
-let App () = React.fragment [
-    Html.div [
-        Html.text "Hello world"
+let Router () =
+    let (currentUrl, updateUrl) = React.useState (Router.currentPath())
+
+    React.router [
+        router.pathMode
+        router.onUrlChanged updateUrl
+        router.children [
+            match currentUrl with
+            | [] -> Html.text "Hello world"
+            | _ -> Html.text "Not Found"
+        ]
     ]
-]
+
 
 ReactDOM
     .createRoot(document.getElementById "root")
-    .render(App())
+    .render(Router())
