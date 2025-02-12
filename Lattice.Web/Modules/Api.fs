4eecd71e0ca376848c2171434266b350b200f9cf
FinOCE <me@itsf.in>
2025-02-12 15:36:06 +1000

    Create api module to implement orchestrator api endpoints for web use

---
+++ Lattice.Web/Modules/Api.fs
@@ -0,0 +1,37 @@
+﻿module Lattice.Web.Api
+
+open System.Net.Http
+open System.Text
+open System.Text.Json
+open System.Text.Json.Nodes
+
+// TODO: Can types be auto-generated from the api project?
+
+module Json =
+    let add (key: string) (value: 'a) (json: JsonObject) =
+        json.Add(key, JsonValue.Create value)
+        json
+
+    let get<'a> (key: string) (json: JsonNode) =
+        json[key].GetValue<'a>()
+
+let [<Literal>] BASE_URL = "http://localhost:7071/api/" // TODO: Configure in environment
+
+let login (code: string) (redirectUri: string) (client: HttpClient) = task {
+    let json =
+        JsonObject()
+        |> Json.add "code" code
+        |> Json.add "redirectUri" redirectUri
+
+    use content = new StringContent(JsonSerializer.Serialize json, Encoding.UTF8, "application/json")
+
+    match! client.PostAsync(BASE_URL + "auth/login", content) with
+    | res when not res.IsSuccessStatusCode -> return Error res.StatusCode
+    | res ->
+        let! body = res.Content.ReadAsStringAsync()
+        let json = JsonObject.Parse body
+
+        return Ok {|
+            Token = json |> Json.get<string> "token"
+        |}
+}
--- Lattice.Web/Pages/Auth.fs
+++ Lattice.Web/Pages/Auth.fs
@@ -3,8 +3,12 @@
 open Browser.Dom
 open Feliz
 open Lattice.Web
+open System.Net.Http
 open System.Security.Cryptography
 open System.Web
+
+let [<Literal>] CLIENT_ID = "1169979466303418368" // TODO: Set from configuration
+let [<Literal>] REDIRECT_URI = "http://localhost:4280/auth/login" // TODO: Set from configuration
 
 [<ReactComponent>]
 let Login () =
@@ -19,9 +23,8 @@ let Login () =
         let authorizeUrl (clientId: string) (redirectUri: string) (scopes: string list) =
             $"""https://discord.com/oauth2/authorize?client_id={clientId}&response_type=code&redirect_uri={HttpUtility.UrlEncode redirectUri}&scope={scopes |> String.concat "+"}"""
             // TODO: Implement this kind of function in FSharp.Discord then use here
-            // TODO: Figure out how to use environment variables to set client ID and redirect URI
 
-        window.location.href <- authorizeUrl "1169979466303418368" "http://localhost:4280/auth/login" ["identify"]
+        window.location.href <- authorizeUrl CLIENT_ID REDIRECT_URI ["identify"]
     ), [||])
 
     Html.div [
@@ -33,27 +36,31 @@ let Callback code state =
     let savedState, dispatch = React.useContext StateContext.context
 
     let loading, setLoading = React.useState true
-    let success, setSuccess = React.useState false
+    let error, setError = React.useState Option<int>.None
 
-    React.useEffect((fun () ->
+    React.useEffect((fun () -> task {
         match state, savedState.State with
         | state, Some saved when state = saved ->
             StateContext.Msg.Clear |> dispatch
 
-            // TODO: Create api client to handle interacting with lattice orchestrator
-            // TODO: Send code to server to exchange for token (then store it in local storage, abstracted away with react context)
+            use client = new HttpClient()
 
-            setSuccess true
+            match! Api.login code REDIRECT_URI client with
+            | Error status ->
+                setError (Some (int status))
+
+            | Ok res ->
+                res.Token |> ignore // TODO: Add to local storage as user context (should the api also return info about the user?)
         | _ -> ()
 
         setLoading false
-    ), [||])
+    }), [||])
 
     let message =
-        match loading, success with
+        match loading, error with
         | true, _ -> "Loading..."
-        | false, false -> "Invalid request"
-        | false, true -> "Success!"
+        | false, Some error -> $"Invalid request: Error {error}"
+        | false, None -> "Success!"
 
     Html.div [
         Html.text message
