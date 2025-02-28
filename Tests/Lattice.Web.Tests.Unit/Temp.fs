f87d5d1817f4fbe1f552c68f5fb3a8b3eb7aca27
FinOCE <me@itsf.in>
2025-02-28 23:35:18 +1000

    Add test projects for contracts and web projects

---
+++ Tests/Lattice.Web.Tests.Unit/Temp.fs
@@ -0,0 +1,9 @@
+﻿namespace Lattice.Web
+
+open Microsoft.VisualStudio.TestTools.UnitTesting
+
+[<TestClass>]
+type Temp () =
+    [<TestMethod>]
+    member _.``Temporary test to ensure MSTest runner success`` () =
+        Assert.IsTrue(true)
