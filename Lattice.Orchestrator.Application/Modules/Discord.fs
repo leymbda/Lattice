55201e49159d1fd0e53f653c4156d21f79a16407
leydel <me@itsf.in>
2025-04-29 22:58:02 +1000

    Refactory register command using fstoolkit error handling

---
+++ Lattice.Orchestrator.Application/Modules/Discord.fs
@@ -0,0 +1,38 @@
+﻿namespace Lattice.Orchestrator.Application
+
+open FSharp.Discord.Types
+open Lattice.Orchestrator.Domain
+
+module PrivilegedIntents =
+    let fromFlags (flags: ApplicationFlag list): PrivilegedIntents =
+        {
+            MessageContent = List.contains ApplicationFlag.GATEWAY_MESSAGE_CONTENT flags
+            MessageContentLimited = List.contains ApplicationFlag.GATEWAY_MESSAGE_CONTENT_LIMITED flags
+            GuildMembers = List.contains ApplicationFlag.GATEWAY_GUILD_MEMBERS flags
+            GuildMembersLimited = List.contains ApplicationFlag.GATEWAY_GUILD_MEMBERS_LIMITED flags
+            Presence = List.contains ApplicationFlag.GATEWAY_PRESENCE flags
+            PresenceLimited = List.contains ApplicationFlag.GATEWAY_PRESENCE_LIMITED flags
+        }
+
+module Team =
+    let fromApplication (application: Application): Team option =
+        match application.Team, application.Owner with
+        | Some team, Some owner ->
+            let applyOwnerRole userId role =
+                match role with
+                | _ when userId = owner.Id -> TeamMemberRole.OWNER
+                | TeamMemberRoleType.ADMIN -> TeamMemberRole.ADMIN
+                | TeamMemberRoleType.DEVELOPER -> TeamMemberRole.DEVELOPER
+                | TeamMemberRoleType.READONLY -> TeamMemberRole.READONLY
+
+            let members =
+                team.Members
+                |> List.map (fun m -> m.User.Id, applyOwnerRole m.User.Id m.Role)
+                |> Map.ofList
+
+            Some {
+                AppId = application.Id
+                Members = members
+            }
+
+        | _ -> None
--- Lattice.Orchestrator.Application/Operations/Applications/RegisterApplicationCommand.fs
+++ Lattice.Orchestrator.Application/Operations/Applications/RegisterApplicationCommand.fs
@@ -1,6 +1,7 @@
 ﻿namespace Lattice.Orchestrator.Application
 
 open FSharp.Discord.Types
+open FsToolkit.ErrorHandling
 open Lattice.Orchestrator.Domain
 open System.Threading.Tasks
 
@@ -15,40 +16,43 @@ type RegisterApplicationCommandError =
     | RegistrationFailed
 
 module RegisterApplicationCommand =
-    let run (env: #ICache & #IDiscord & #IPersistence & #ISecrets) (props: RegisterApplicationCommandProps) = task {
+    let run (env: #ICache & #IDiscord & #IPersistence & #ISecrets) props = asyncResult {
         // Validate application with Discord
-        match! env.GetApplicationInformation props.DiscordBotToken with
-        | None -> return Error RegisterApplicationCommandError.InvalidBotToken
-        | Some application ->
-
+        let! application =
+            env.GetApplicationInformation props.DiscordBotToken
+            |> Async.AwaitTask
+            |> AsyncResult.requireSome InvalidBotToken
+            
         // Ensure user has access to application
-        let members = Map.empty<string, TeamMemberRole> // TODO: Map app owner/team-members into here (requires new FSharp.Discord version)
-        let team = Team.create application.Id members // TODO: This is a duplicate of code in the Cache composite, this should all be refactored to be neat
+        let! team =
+            Team.fromApplication application
+            |> Result.requireSome RegistrationFailed
 
-        match team.Members.TryFind props.UserId with
-        | None -> return Error RegisterApplicationCommandError.Forbidden
-        | Some _ ->
+        do!
+            team.Members.ContainsKey props.UserId
+            |> Result.requireTrue Forbidden
 
         // Create application and save to db
-        let encryptedBotToken = props.DiscordBotToken |> Aes.encrypt env.BotTokenEncryptionKey
+        let encryptedBotToken =
+            props.DiscordBotToken
+            |> Aes.encrypt env.BotTokenEncryptionKey
 
         let privilegedIntents =
-            {
-                MessageContent = application.Flags |> Option.defaultValue [] |> List.contains ApplicationFlag.GATEWAY_MESSAGE_CONTENT
-                MessageContentLimited = application.Flags |> Option.defaultValue [] |> List.contains ApplicationFlag.GATEWAY_MESSAGE_CONTENT_LIMITED
-                GuildMembers = application.Flags |> Option.defaultValue [] |> List.contains ApplicationFlag.GATEWAY_GUILD_MEMBERS
-                GuildMembersLimited = application.Flags |> Option.defaultValue [] |> List.contains ApplicationFlag.GATEWAY_GUILD_MEMBERS_LIMITED
-                Presence = application.Flags |> Option.defaultValue [] |> List.contains ApplicationFlag.GATEWAY_PRESENCE
-                PresenceLimited = application.Flags |> Option.defaultValue [] |> List.contains ApplicationFlag.GATEWAY_PRESENCE_LIMITED
-            }
+            application.Flags
+            |> Option.defaultValue []
+            |> PrivilegedIntents.fromFlags
 
-        let app = App.create application.Id encryptedBotToken privilegedIntents
+        let! app =
+            App.create application.Id encryptedBotToken privilegedIntents
+            |> env.SetApp
+            |> Async.AwaitTask
+            |> AsyncResult.setError RegistrationFailed
 
-        match! env.SetApp app with
-        | Error _ -> return Error RegisterApplicationCommandError.RegistrationFailed
-        | Ok app ->
+        // Save team to cache
+        do!
+            env.SetTeam team
+            |> Task.wait
 
-        // Update team in cache
-        do! env.SetTeam team :> Task
-        return Ok app
+        // Return newly registered app
+        return app
     }
